761980fd5a1742feb37b8598b548c6322151c342
Impp18 <tdfallara@gmail.com>
2022-11-09 14:12:59 -0500

    made anchor tags, attempted to make id

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,16 @@
 
 var ingredientSearchItem = 'onion';
 var recipeSearchItem = 'onion rings';
-var ingredientsOfRecepieSearchItem = '';
 
 function displayIngredients(){
-  // var ingredientArea = document.getElementById('fullRecipe');
-  // recepieName = document.createElement('h2');
-  // recepieName.textContent = data.hits[i].recipe.label;
-  // recepieIngredientList = document.createElement('ul');
-  // var li = document.createElement('li');
-  // li.textContent = data.hits[i].recipe.ingredientLines;
+
   console.log('reeeeeeeeeeeeeeeeeeeeeeeeee')
 }
 
 //grabs recepies based on single item input
 function getRecipesByIngredient(){
 
-    var requestUrl = 'https://api.edamam.com/search?q='+ingredientSearchItem+'&app_id=b958af72&app_key=46b1301f63cc0535dde1e7187e2ff26b';
+    var requestUrl = 'https://russelldev-cors-anywhere.herokuapp.com/https://api.edamam.com/search?q='+ingredientSearchItem+'&app_id=b958af72&app_key=46b1301f63cc0535dde1e7187e2ff26b';
 
 fetch(requestUrl)
     .then(function (response) {
@@ -26,27 +20,30 @@ fetch(requestUrl)
     .then(function (data) {
       //targeting the list by id. in this case the placeholder is, recepieList1
         let list = document.getElementById("recipeList1");
-        var recipeLog = document.getElementById('hiddenRecipeInformation');       
+        var recipeLog = document.getElementById('recipeInformation');       
         console.log(data.q);
 
-      //makes list of recipes that use the item in the search function and consloe logs for good measure
+      //makes list of recipes that use the item in the search function and console logs for good measure
       for(i=0; i < data.hits.length; i++) {
       console.log(data.hits[i].recipe.label);
-
-      
+      currentLabel = data.hits[i].recipe.label;
       var li = document.createElement('li');
-      li.textContent = data.hits[i].recipe.label;
+      var a = document.createElement('a');
+      a.href = "#"+currentLabel;
+      a.textContent = currentLabel;
       recipeName = document.createElement('h2');
-      recipeName.textContent = data.hits[i].recipe.label;
+      recipeName.textContent = currentLabel;
+      li.appendChild(a);
       list.appendChild(li);
-      var recipeIngredientList = document.createElement('ul'); 
-         // adding ingredients to the second list
+
+      var recipeIngredientList = document.createElement('ul');
+      // adding ingredients to the second list
           for(y=0; y <data.hits[i].recipe.ingredients.length; y++){
             var li2 = document.createElement('li');
             console.log(data.hits[i].recipe.ingredients[y].text)
             li2.textContent = data.hits[i].recipe.ingredients[y].text;
             recipeIngredientList.appendChild(li2)
-      }
+      } 
       recipeName.appendChild(recipeIngredientList)
       recipeLog.appendChild(recipeName)
       
@@ -60,7 +57,7 @@ fetch(requestUrl)
 
 function getRecipesByName(){
 
-    var requestUrl = 'https://api.edamam.com/search?q='+recipeSearchItem+'&app_id=b958af72&app_key=46b1301f63cc0535dde1e7187e2ff26b';
+    var requestUrl = 'https://russelldev-cors-anywhere.herokuapp.com/https://api.edamam.com/search?q='+recipeSearchItem+'&app_id=b958af72&app_key=46b1301f63cc0535dde1e7187e2ff26b';
 
 fetch(requestUrl)
     .then(function (response) {
@@ -69,16 +66,20 @@ fetch(requestUrl)
     .then(function (data) {
       //targeting the list by id. in this case the placeholder is, recepieList2     
         let list = document.getElementById("recipeList2");
-        var recipeLog = document.getElementById('hiddenRecipeInformation');       
+        var recipeLog = document.getElementById('recipeInformation');       
         console.log(data);
         
         //makes list of recipes that use the item in the search function then use that to make another list to add ingredients to
         for(i=0; i < data.hits.length; i++) {
-          
+          currentLabel = data.hits[i].recipe.label;
           var li = document.createElement('li');
-          li.textContent = data.hits[i].recipe.label;
+          var a = document.createElement('a');
+          a.href = "#"+currentLabel;
+          a.textContent = currentLabel;
           recipeName = document.createElement('h2');
-          recipeName.textContent = data.hits[i].recipe.label;
+          recipeName.id.add('#'+currentLabel)
+          recipeName.textContent = currentLabel;
+          li.appendChild(a);
           list.appendChild(li);
           var recipeIngredientList = document.createElement('ul'); 
          // adding ingredients to the second list
@@ -100,30 +101,5 @@ fetch(requestUrl)
     });
 }
 
-// function getIngredients(){
-  
-  //     var requestUrl = 'https://api.edamam.com/search?q='+recipeSearchItem+'&app_id=b958af72&app_key=46b1301f63cc0535dde1e7187e2ff26b';
-  
-  // fetch(requestUrl)
-  //     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {      
-  //       //targeting the list by id. in this case the placeholder is, ingredientList
-  //       let list = document.getElementById("ingredientList");
-  
-  //         console.log(data);
-  
-  //       //makes list of recipes that use the item in the search function and consloe logs for good measure
-  //       for(i=0; i < data.hits.length; i++) {
-    
-    //       var li = document.createElement('li');
-    //       li.textContent = data.hits[i].recipe.label;
-    //       list.appendChild(li)
-    //       }
-    //     });
-// }
-
 getRecipesByIngredient()
 getRecipesByName()
-// getIngredients()
